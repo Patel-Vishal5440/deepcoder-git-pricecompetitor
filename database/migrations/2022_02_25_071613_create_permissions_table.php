40a9b07ed1401c4539707745389b6db587e71550
Patel-Vishal5440 <vishal.patel@deepcoder.io>
2025-07-15 18:38:58 +0530

    migration issue fix
    migration issue fix

--- database/migrations/2022_02_25_071613_create_permissions_table.php
+++ database/migrations/2022_02_25_071613_create_permissions_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->string('group')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
